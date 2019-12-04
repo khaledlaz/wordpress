a3eb5848d94e88dd8adb44955acd82312855e511
Khaled Laz <58021319+khaledlaz@users.noreply.github.com>
2019-12-04 08:48:38 +0600

    Add files via upload

---
+++ db/v1.0.sql
@@ -0,0 +1,3 @@
+insert into wp_comments (comment_post_id, comment_author, comment_content) values (6,  'khalidlaz',  'insert from sql script');
+insert into wp_comments (comment_post_id, comment_author, comment_content) values (6,  'khalidlaz',  'insert from sql script');
+insert into wp_comments (comment_post_id, comment_author, comment_content) values (6,  'khalidlaz',  'insert from sql script');
